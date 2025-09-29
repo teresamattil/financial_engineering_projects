a2a66e8f9622fec0cb457bb5f7a0d8fddcd2a83f
Teresa <teresa.mattil@gmail.com>
2025-09-29 15:02:40 +0200

    PLotting financial data

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-import os
+import yfinance as yf
 import pandas as pd
+import numpy as np
+import datetime as date
+import matplotlib.pyplot as plt
 
-print("hola")
+startdate = date.datetime(2000, 1, 1)
+enddate = date.datetime.today()
+
+# Use yfinance instead of pandas_datareader
+ipc = yf.download('^MXX', start=startdate, end=enddate)['Close']
+
+ipc.plot()
